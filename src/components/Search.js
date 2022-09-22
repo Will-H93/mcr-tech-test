2ac3b1c44d21430002c3f92b9ef6399d94ecfb80
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-09-22 21:10:59 +0100

    added search form and css

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,20 @@ import React, { useState } from "react";
 import "../styles/search.css";
 
 const Search = () => {
+  const [value, setValue] = useState();
+
   return (
     <>
-      <input className="search-input" type="text" />
+    <form className="search-form">
+        <input
+          className="search-input"
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+      <button className="search-button" type="submit">
+        Search
+      </button>
+    </form>
     </>
   );
 };

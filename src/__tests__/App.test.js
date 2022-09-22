1faa621e5deade17837b63f8ed74c6f3648b86fe
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-09-22 20:48:19 +0100

    added search component and tested that it renders

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,2 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../components/App";
+
+describe("App", () => {
+  const { asFragment } = render(<App />);
+
+  test("renders correctly", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React from "react"
-import './styles/app.css';
+import React from "react";
+import Search from "./Search";
+import "../styles/app.css";
 
 const App = () => {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <img
+        className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="nasaLogo"
+      />
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
